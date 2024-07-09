bf85bfb224ad6897835f017b4197b229c14daf24
Maryia Stroich <stroichmwork@gmail.com>
2024-07-09 16:06:55 +0400

    test: add search test

---
+++ src/components/Search/Search.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {fireEvent, render} from '@testing-library/react-native';
+import Search from './Search';
+
+jest.mock('react-native-safe-area-context', () => {
+  const inset = {top: 0, right: 0, bottom: 0, left: 0};
+  return {
+    SafeAreaProvider: jest.fn().mockImplementation(({children}) => children),
+    SafeAreaConsumer: jest
+      .fn()
+      .mockImplementation(({children}) => children(inset)),
+    useSafeAreaInsets: jest.fn().mockImplementation(() => inset),
+  };
+});
+
+const mockGoBack = jest.fn();
+
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => {
+    const navigation = {
+      goBack: mockGoBack,
+    };
+    return navigation;
+  },
+}));
+
+jest.mock('react-native-vector-icons/AntDesign', () => 'AntDesign');
+
+describe('renders Search', () => {
+  it('render Search', () => {
+    const {getByPlaceholderText} = render(<Search onSearch={() => {}} />);
+    getByPlaceholderText('Search');
+  });
+
+  it('should call onSearch when the inpun is submitted', () => {
+    const mockGOnSearch = jest.fn();
+    const {getByPlaceholderText} = render(<Search onSearch={mockGOnSearch} />);
+    const input = getByPlaceholderText('Search');
+    fireEvent(input, 'submitEditing', {nativeEvent: {text: 'some text'}});
+    expect(mockGOnSearch).toHaveBeenCalled();
+  });
+
+  it('should go to back when the button is pressed', () => {
+    const mockGOnSearch = jest.fn();
+    const {getByTestId} = render(<Search onSearch={mockGOnSearch} />);
+    const backButton = getByTestId('SearchPage-backButton');
+    fireEvent.press(backButton);
+    expect(mockGoBack).toHaveBeenCalled();
+  });
+});
