495c52073c14f05eafa162d7cda89d333d696958
Maryia Stroich <stroichmwork@gmail.com>
2024-07-23 18:35:12 +0400

    docs: add setting for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,32 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  env: {
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: [
+    '@react-native',
+    'prettier',
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'plugin:react-native/all',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier', 'react', 'react-native', 'jest'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
+    'prettier/prettier': 'error',
     '@typescript-eslint/no-unused-vars': ['error'],
+    'react-native/sort-styles': 'off',
+    'react-native/no-raw-text': ['error', {skip: ['ThemedText']}],
+    'react-native/no-unused-styles': 'off',
   },
 };
