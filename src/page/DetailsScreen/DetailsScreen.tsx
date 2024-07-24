184d3d826bf18d60da408bb96e7c4d88fd924a9d
Maryia Stroich <stroichmwork@gmail.com>
2024-07-24 14:32:05 +0400

    refactor: refactor DetailsScreen

--- src/page/DetailsScreen/DetailsScreen.tsx
+++ src/page/DetailsScreen/DetailsScreen.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import {DetailsScreenProps} from '../../types/navigationTypes';
-import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
-import CardDetail from '../../components/CardDetail/CardDetail';
+import {DetailsScreenProps} from '@type/navigationTypes';
+import ErrorMessage from '@components/ErrorMessage/ErrorMessage';
+import CardDetail from '@components/CardDetail/CardDetail';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import Loading from '../../components/Loading/Loading';
+import Loading from '@components/Loading/Loading';
+
 import {useQuery} from '@tanstack/react-query';
-import {getMovie} from '../../utils/api/apiMovies';
+import {getMovie} from '@utils/api/apiMovies';
 import {useSnapshot} from 'valtio';
-import themeState from '../../store/GlobalStores/themeState';
+import themeState from '@store/GlobalStores/themeState';
 
 function DetailsScreen({route}: DetailsScreenProps) {
   const {colors} = useSnapshot(themeState);
